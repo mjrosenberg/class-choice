c31c98a22db94859e0e9120cfe756ff25b683e22
mjrosenberg <mjr70@duke.edu>
2020-07-25 10:44:23 -0700

    mongo schema implemented

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -3,29 +3,49 @@ mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
 
-db.on('error', function() {
-  console.log('mongoose connection error');
+db.on('error', function(err) {
+  console.log('mongoose connection error', err);
 });
 
 db.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
-var itemSchema = mongoose.Schema({
-  quantity: Number,
-  description: String
+const subjects = ['Math', 'Computer Science', 'History', 'English'];
+var classSchema = mongoose.Schema({
+  title: {type: String, unique: true, required: true},
+  subject: {type: String, index: true, enum: subjects, required: true},
+  description: {type: String, required: true},
+  students: { type: Array, default: [] }
 });
 
-var Item = mongoose.model('Item', itemSchema);
+var Class = mongoose.model('Class', classSchema);
 
 var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
+  Class.find({}, function(err, classes) {
     if(err) {
       callback(err, null);
     } else {
-      callback(null, items);
+      callback(null, classes);
     }
   });
 };
+// Class.insertMany([
+//   {title: 'Javascript Fundamentals',
+//   subject: 'Computer Science',
+//   description: 'Let\'s learn the basics of javascript'},
+//   {title: 'React and Express',
+//   subject: 'Computer Science',
+//   description: 'Learn basics of full stack development'},
+//   {title: 'Java Fundamentals',
+//   subject: 'Computer Science',
+//   description: 'Let\'s learn the basics of java'},
+//   {title: 'Design and Analysis oof Algorithms',
+//   subject: 'Computer Science',
+//   description: 'Design and analyze coomplex algorithms through proof-based learning'},
+// ],
+// (err)=> {
+//   console.log(err);
+// });
 
 module.exports.selectAll = selectAll;
